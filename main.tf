1c04d2afccd0ef6802b282a6898f60c666dc9cd0
Sergio Paulo <55364041+SPauloG@users.noreply.github.com>
2023-03-22 14:19:03 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,42 +1,46 @@
-provider "googleworkspace" {
-  credentials             = "${file("serviceaccount.yaml")}"
-  customer_id             = "informaraqui"
-  impersonated_user_email = "emailaqui"
-  oauth_scopes = [
-    "https://www.googleapis.com/auth/admin.directory.user",
-    "https://www.googleapis.com/auth/admin.directory.userschema",
-    "https://www.googleapis.com/auth/admin.directory.group",	
-    # include scopes as needed
-  ]
+provider "google" {
+  project = "sergiopaulo-devops-iac"
+  region  = "us-west1"
+  zone    = "us-west1-c"
+  credentials = "${file("serviceaccount.yaml")}"
 }
 
-resource "googleworkspace_group" "devops" {
-  email       = "devops@carlosbarbero.com.br"
-  name        = "Devops"
-  description = "Devops Group"
-
-  aliases = ["dev-ops@carlosbarbero.com.br"]
-
-  timeouts {
-    create = "1m"
-    update = "1m"
-  }
+resource "google_folder" "Comercial" {
+  display_name = "Comercial"
+  parent       = "organizations/12345678910"
 }
 
-resource "googleworkspace_user" "joao" {
-  primary_email = "joao@carlosbarbero.com.br"
-  password      = "34819d7beeabb9260a5c854bc85b3e44"
-  hash_function = "MD5"
-
-  name {
-    family_name = "Joao"
-    given_name  = "Ninguem"
-  }
+resource "google_folder" "Web" {
+  display_name = "Web"
+  parent       = google_folder.Comercial.name
 }
 
-resource "googleworkspace_group_member" "manager" {
-  group_id = googleworkspace_group.devops.id
-  email    = googleworkspace_user.joao.primary_email
+resource "google_folder" "Desenvolvimento" {
+  display_name = "Desenvolvimento"
+  parent       = google_folder.Web.name
+}
 
-  role = "MANAGER"
+resource "google_folder" "Producao" {
+  display_name = "Producao"
+  parent       = google_folder.Web.name
+}
+
+
+resource "google_project" "sergiopaulo-web-dev" {
+  name       = "Web-Dev"
+  project_id = "sergiopaulo-web-dev"
+  folder_id  = google_folder.Desenvolvimento.name
+  auto_create_network = false
+  billing_account = "12345-54321-123456"
+
+}
+
+
+resource "google_project" "sergiopaulo-web-prod" {
+  name       = "Web-Prod"
+  project_id = "sergiopaulo-web-prod"
+  folder_id  = google_folder.Producao.name
+  auto_create_network = false
+  billing_account = "12345-54321-123456"
+
 }
